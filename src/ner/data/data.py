585bef2030683a96029e8f24654b1e2baf660240
AnnurAfgoni <afgoniannur@gmail.com>
2024-12-11 06:54:00 +0800

    feat: implement token module and create datamodule class

--- src/ner/data/data.py
+++ src/ner/data/data.py
@@ -1,19 +1,25 @@
+import os
 import torch
-import torch.nn as nn
-
-from collections import Counter
+import lightning as L
 from torch.utils.data import Dataset, DataLoader
-from torch.nn.utils.rnn import pad_sequence
+
+from ner.transform import Token
 
 
-class NERDataset(Dataset):
-    def __init__(self, data_path):
+class ConllDataset(Dataset):
+    def __init__(
+        self, 
+        data_path,
+        token_manager,
+        ner_manager, 
+        max_len: int=100
+    ):
         self.data_path = data_path
-        
-        self.data = []
+        self.max_len = max_len
+        self.token_manager = token_manager
+        self.ner_manager = ner_manager
 
-        self._load_data()
-        self._get_mapper()
+        self.data = self._load_data()
 
     def _read_data(self):
         with open(self.data_path) as f:
@@ -22,6 +28,7 @@ class NERDataset(Dataset):
 
     def _load_data(self):
         raw_data = self._read_data()
+        data = []
         data_dict = None
         idx = 0
 
@@ -30,55 +37,96 @@ class NERDataset(Dataset):
                 continue
             if line.strip() == "":
                 if data_dict:
-                    self.data.append(data_dict)
+                    data.append(data_dict)
                 data_dict = None
             else:
                 if data_dict is None:
-                    data_dict = {
-                        "id": idx,
-                        "tokens": [],
-                        "ner_outputs": []
-                    }
+                    data_dict = {"id": idx, "tokens": [], "ner_outputs": []}
                     idx += 1
                 l = line.split()
                 data_dict["tokens"].append(f"{l[0]}_{l[1]}")
                 data_dict["ner_outputs"].append(l[3])
+        return data
 
-    def build_vocab(self, sentences, specials=None, special_first=True):
-        
-        token_counter = Counter(token for token in sentences)
-        sorted_tokens = sorted(token_counter.keys(), key=lambda x: (-token_counter[x], x))
-
-        if specials:
-            if special_first:
-                sorted_tokens = specials + sorted_tokens
-            else:
-                sorted_tokens = sorted_tokens + specials
-
-        vocab = {token: idx for idx, token in enumerate(sorted_tokens)}
-        return vocab
-
-    def encode(self, tokens, vocab):
-        return [vocab.get(token, vocab["<unk>"]) for token in tokens]
-    
-    def _get_mapper(self):
-        sentences = []
-        ner_outputs = []
-
-        for data in self.data:
-            sentences.extend(data["tokens"])
-            ner_outputs.extend(data["ner_outputs"])
-
-        self.vocab = self.build_vocab(sentences, specials=["<unk>", "<pad>"])
-        self.ner_vocab = self.build_vocab(ner_outputs, specials=["<unk>", "<pad>"])
+    def _pad_sequence(self, sequence, pad_idx):
+        sequence = sequence[:self.max_len]
+        return sequence + [pad_idx] * (self.max_len - len(sequence))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        
-        token = torch.tensor(self.encode(self.data[idx]["tokens"], self.vocab), dtype=torch.long)
-        target = torch.tensor(self.encode(self.data[idx]["ner_outputs"], self.ner_vocab), dtype=torch.long)
+        sample = self.data[idx]
 
-        return token, target
+        token = self.token_manager.encode(sample["tokens"])
+        ner_tags = self.ner_manager.encode(sample["ner_outputs"])
+
+        token = self._pad_sequence(token, self.token_manager.vocab["<pad>"])
+        ner_tags = self._pad_sequence(ner_tags, self.ner_manager.vocab["<pad>"])        
         
+        return torch.tensor(token, dtype=torch.long), torch.tensor(ner_tags, dtype=torch.long)
+        
+
+class ConllDataModule(L.LightningDataModule):
+    def __init__(
+        self, 
+        data_path, 
+        max_len: int=100
+    ):
+        super().__init__()
+
+        self.data_path = data_path
+        self.max_len = max_len
+
+    def setup(self, stage=None):
+        train_path = os.path.join(self.data_path, "train.txt")
+        val_path = os.path.join(self.data_path, "valid.txt")
+        test_path = os.path.join(self.data_path, "test.txt")
+
+        token_manager = Token()
+        ner_manager = Token()
+
+        self.train_dataset = ConllDataset(
+            train_path, 
+            token_manager, 
+            ner_manager, 
+            self.max_len
+        )
+
+        token_manager.build_vocab(self.train_dataset.data, is_token=True)
+        ner_manager.build_vocab(self.train_dataset.data, is_token=False)
+
+        self.val_dataset = ConllDataset(
+            val_path, 
+            token_manager, 
+            ner_manager, 
+            self.max_len
+        )
+        self.test_dataset = ConllDataset(
+            test_path, 
+            token_manager, 
+            ner_manager, 
+            self.max_len
+        )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset, 
+            batch_size=32, 
+            num_workers=11, 
+            shuffle=True
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset, 
+            batch_size=32, 
+            num_workers=11
+        )
+    
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset, 
+            batch_size=32, 
+            num_workers=11
+        )
